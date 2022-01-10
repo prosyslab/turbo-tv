5f76ae5ea7232cba736bab3dffe9fbd5bea81cfe
seungwan.kwon <seungwan.kwon@prosys.kr>
2022-01-10 10:55:49 +0900

    [build] add build script

--- Makefile
+++ Makefile
@@ -3,3 +3,5 @@ all:
 	ln -sf _build/default/src/main.exe jstv
 clean:
 	dune clean
+	rm -f ./jstv
+	rm -f *.dot
---
+++ build.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
+OCAML_VERSION="4.13.1"
+JSTV_OPAM_SWITCH="jstv-4.13.1"
+opam init --compiler=$OCAML_VERSION -j $NCPU --no-setup
+
+switch_exists=no
+for installed_switch in $(opam switch list --short); do
+  if [[ "$installed_switch" == "$JSTV_OPAM_SWITCH" ]]; then
+    switch_exists=yes
+    break
+  fi
+done
+
+if [ "$switch_exists" = "no" ]; then
+  opam switch create $JSTV_OPAM_SWITCH $OCAML_VERSION
+else
+  opam switch $JSTV_OPAM_SWITCH
+fi
+
+eval $(SHELL=bash opam env --switch=$JSTV_OPAM_SWITCH)
+opam install dune ocamlgraph
+make
