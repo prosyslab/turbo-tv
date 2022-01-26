e85fa40f2c762461e4d870e0369beb898f279d9b
seungwan.kwon <seungwan.kwon@prosys.kr>
2022-01-26 23:32:01 +0900

    [err] add the common error module

---
+++ src/err.ml
@@ -0,0 +1,19 @@
+type e = string * string
+
+exception TypeMismatch of e
+exception NodeNotFound of e
+exception IdNotFound of e
+exception InvalidGraphLine of e
+exception InvalidIndex of e
+
+let err excpt =
+  (match excpt with
+  | TypeMismatch (c, r) -> Printf.fprintf stderr "Type mismatch: %s\n%s\n\n" c r
+  | InvalidGraphLine (c, r) ->
+      Printf.fprintf stderr "Invalid graph line: %s\n%s\n\n" c r
+  | NodeNotFound (c, r) ->
+      Printf.fprintf stderr "Node not found: %s\n%s\n\n" c r
+  | IdNotFound (c, r) -> Printf.fprintf stderr "Id not found: %s\n%s\n\n" c r
+  | InvalidIndex (c, r) -> Printf.fprintf stderr "Invalid index: %s\n%s\n\n" c r
+  | _ -> ());
+  raise excpt
