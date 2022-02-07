4a4d2237caa48464f38430c4f7925b522c684197
seungwan.kwon <seungwan.kwon@prosys.kr>
2022-02-07 16:05:07 +0900

    [z3utils] support default bitvector operation

--- src/err.ml
+++ src/err.ml
@@ -1,6 +1,7 @@
 type e = string * string
 
 exception TypeMismatch of e
+exception InvalidFormat of e
 exception NodeNotFound of e
 exception IdNotFound of e
 exception InvalidGraphLine of e
@@ -9,11 +10,13 @@ exception InvalidIndex of e
 let err excpt =
   (match excpt with
   | TypeMismatch (c, r) -> Printf.fprintf stderr "Type mismatch: %s\n%s\n\n" c r
-  | InvalidGraphLine (c, r) ->
-      Printf.fprintf stderr "Invalid graph line: %s\n%s\n\n" c r
+  | InvalidFormat (c, r) ->
+      Printf.fprintf stderr "Invalid Format: %s\n%s\n\n" c r
   | NodeNotFound (c, r) ->
       Printf.fprintf stderr "Node not found: %s\n%s\n\n" c r
   | IdNotFound (c, r) -> Printf.fprintf stderr "Id not found: %s\n%s\n\n" c r
+  | InvalidGraphLine (c, r) ->
+      Printf.fprintf stderr "Invalid graph line: %s\n%s\n\n" c r
   | InvalidIndex (c, r) -> Printf.fprintf stderr "Invalid index: %s\n%s\n\n" c r
   | _ -> ());
   raise excpt
---
+++ src/z3utils.ml
@@ -0,0 +1,90 @@
+open Z3
+open Err
+module B = Boolean
+module BV = BitVector
+
+let ctx = mk_context [ ("model", "true"); ("unsat_core", "true") ]
+let str_of_expr expr = expr |> Expr.to_string
+let simplify opt expr = Expr.simplify expr opt
+let bvlen = ref 64
+let set_bvlen len = bvlen := len
+
+module BitVecVal = struct
+  type t = Expr.expr
+
+  (* numeral const *)
+  let zero ?(len : int = !bvlen) = BV.mk_numeral ctx "0" len
+
+  (* constructor *)
+  let of_int ?(len = !bvlen) value =
+    BV.mk_numeral ctx (value |> string_of_int) len
+
+  let of_str ?(len = !bvlen) str =
+    let dec_str =
+      try Int64.of_string str |> Int64.to_string
+      with Failure _ ->
+        let c = str in
+        let r =
+          "%s has a invalid format or exceeds the range of integers \
+           representable in type `int64`"
+        in
+        err (InvalidFormat (c, r))
+    in
+
+    BV.mk_numeral ctx dec_str len
+
+  (* boolean const *)
+  let t ?(len = !bvlen) () = BV.mk_numeral ctx "1" len
+  let f ?(len = !bvlen) () = BV.mk_numeral ctx "0" len
+end
+
+module BitVec = struct
+  type t = Expr.expr
+
+  let init ?(len = !bvlen) name = BV.mk_const_s ctx name len
+  let len bv = bv |> Expr.get_sort |> BV.get_size
+
+  (* logical operation *)
+  let andb lbv rbv = BV.mk_and ctx lbv rbv
+
+  let andi lbv rval =
+    let rbv = BitVecVal.of_int ~len:(len lbv) rval in
+    BV.mk_and ctx lbv rbv
+
+  let orb lbv rbv = BV.mk_or ctx lbv rbv
+
+  let ori lbv rval =
+    let rbv = BitVecVal.of_int ~len:(len lbv) rval in
+    BV.mk_or ctx lbv rbv
+
+  let notb bv = BV.mk_not ctx bv
+  let shlb bv off = BV.mk_shl ctx bv off
+
+  let shli bv off =
+    let rbv = BitVecVal.of_int ~len:(len bv) off in
+    BV.mk_shl ctx bv rbv
+
+  (* comparison *)
+  let eqb lbv rbv = B.mk_eq ctx lbv rbv
+
+  let eqi lbv rval =
+    let rbv = BitVecVal.of_int ~len:(len lbv) rval in
+    B.mk_eq ctx lbv rbv
+
+  let neqb lbv rbv = B.mk_not ctx (eqb lbv rbv)
+  let neqi lbv rval = B.mk_not ctx (eqi lbv rval)
+  let ugeb lbv rbv = BV.mk_uge ctx lbv rbv
+
+  let ugei lbv rval =
+    let rbv = BitVecVal.of_int ~len:(len lbv) rval in
+    BV.mk_uge ctx lbv rbv
+
+  (* boolean operation *)
+  let is_true bv =
+    let fbv = BitVecVal.f ~len:(len bv) () in
+    neqb bv fbv
+
+  let is_false bv =
+    let fbv = BitVecVal.f ~len:(len bv) () in
+    eqb bv fbv
+end
