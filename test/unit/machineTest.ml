63396e7d6b17b75121fe0930497f75c6061782d0
Hhro <seungwan.kwon@prosys.kr>
2022-09-27 15:32:45 +0900

    [test] unit test for ChangeFloat64ToInt32

--- test/unit/machineTest.ml
+++ test/unit/machineTest.ml
@@ -5,6 +5,7 @@ let conversion_test i_ty o_ty desc input expected =
   let ty, eq, convert =
     match (i_ty, o_ty) with
     | "float64", "int64" -> (Type.int64, Int64.eq, change_float64_to_int64)
+    | "float64", "int32" -> (Type.int32, Int32.eq, change_float64_to_int32)
     | _ -> failwith "not implemented"
   in
   let msg = "\027[91m" ^ name ^ "\027[0m" in
@@ -24,8 +25,43 @@ let change_float64_to_int64_neg_val =
 let change_float64_to_int64_pos_val =
   conversion_test "float64" "int64" "pos_val" 1.0 1
 
+let change_float64_to_int32_neg_val =
+  conversion_test "float64" "int32" "neg_val" (-1.0) (-1)
+
+let change_float64_to_int32_pos_val =
+  conversion_test "float64" "int32" "pos_val" 1.0 1
+
+let change_float64_to_int32_pos_ovf =
+  conversion_test "float64" "int32" "pos_ovf" 2147483648.0 (-2147483648)
+
+let change_float64_to_int32_pos_ovf2 =
+  conversion_test "float64" "int32" "pos_ovf2" 4294967295.0 (-1)
+
+let change_float64_to_int32_pos_ovf3 =
+  conversion_test "float64" "int32" "pos_ovf3" 4294967297.0 1
+
+let change_float64_to_int32_neg_udf =
+  conversion_test "float64" "int32" "neg_udf" (-2147483648.0) (-2147483648)
+
+let change_float64_to_int32_neg_udf2 =
+  conversion_test "float64" "int32" "neg_udf2" (-4294967295.0) 1
+
+let change_float64_to_int32_neg_udf3 =
+  conversion_test "float64" "int32" "neg_udf2" (-4294967297.0) (-1)
+
 let suite =
   "Test suite for machine-level operator semantics"
-  >::: [ change_float64_to_int64_pos_val; change_float64_to_int64_neg_val ]
+  >::: [
+         change_float64_to_int32_pos_val;
+         change_float64_to_int32_neg_val;
+         change_float64_to_int32_pos_ovf;
+         change_float64_to_int32_pos_ovf2;
+         change_float64_to_int32_pos_ovf3;
+         change_float64_to_int32_neg_udf;
+         change_float64_to_int32_neg_udf2;
+         change_float64_to_int32_neg_udf3;
+         change_float64_to_int64_pos_val;
+         change_float64_to_int64_neg_val;
+       ]
 
 let _ = OUnit2.run_test_tt_main suite
