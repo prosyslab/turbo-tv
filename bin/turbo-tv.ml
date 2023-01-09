3cbbbb8c5d23a387b1a9b0d2d647aa5f9beb8e2a
Hhro <seungwan.kwon@prosys.kr>
2023-01-09 14:53:29 +0900

    [turbo-tv] simplify input interface

--- bin/turbo-tv.ml
+++ bin/turbo-tv.ml
@@ -2,28 +2,22 @@ open Cmdliner
 open Lib
 
 type conf = {
-  source : string option;
-  target : string option;
-  check_ub : bool;
+  verify : string;
+  check_ub : string;
   check_type : bool;
   emit_graph : bool;
 }
 
 let jstv_args =
   (* Arguments *)
-  let source_arg =
-    let doc = "Source IR" in
-    Arg.(value & pos 0 (some string) None & info [] ~docv:"SRC" ~doc)
-  in
-
-  let target_arg =
-    let doc = "Target IR" in
-    Arg.(value & pos 1 (some string) None & info [] ~docv:"TGT" ~doc)
+  let verify_arg =
+    let doc = "Reduction" in
+    Arg.(value & opt dir "" & info [ "verify" ] ~docv:"RDC" ~doc)
   in
 
   let check_ub_arg =
-    let doc = "Check whether our system emits UB for the [SRC]" in
-    Arg.(value & flag & info [ "check-ub" ] ~doc)
+    let doc = "Check whether our system emits UB for the [IR]" in
+    Arg.(value & opt file "" & info [ "check-ub" ] ~doc)
   in
 
   let disable_type_check_arg =
@@ -36,13 +30,13 @@ let jstv_args =
     Arg.(value & flag & info [ "emit-graph" ] ~doc)
   in
 
-  let mk_conf source target check_ub check_type emit_graph =
-    { source; target; check_ub; check_type; emit_graph }
+  let mk_conf verify check_ub check_type emit_graph =
+    { verify; check_ub; check_type; emit_graph }
   in
 
   Term.(
-    const mk_conf $ source_arg $ target_arg $ check_ub_arg
-    $ disable_type_check_arg $ emit_graph_arg)
+    const mk_conf $ verify_arg $ check_ub_arg $ disable_type_check_arg
+    $ emit_graph_arg)
 
 let parse_command_line () =
   let doc = "Translation validation for TurboFan IR" in
@@ -103,14 +97,12 @@ let unknown_op_exists graph =
 
 let main () =
   Printexc.record_backtrace true;
-  let { source; target; check_ub; check_type; emit_graph } =
-    parse_command_line ()
-  in
+  let { verify; check_ub; check_type; emit_graph } = parse_command_line () in
   let nparams = 2 in
 
-  if not check_ub then (
-    let src_ir_p = source |> Option.get in
-    let tgt_ir_p = target |> Option.get in
+  if String.length verify <> 0 then (
+    let src_ir_p = Filename.concat verify "src.ir" in
+    let tgt_ir_p = Filename.concat verify "tgt.ir" in
     let src_grp = IR.create_from_ir_file src_ir_p in
     let tgt_grp = IR.create_from_ir_file tgt_ir_p in
 
@@ -121,10 +113,11 @@ let main () =
     if unknown_op_exists src_grp || unknown_op_exists tgt_grp then
       Printf.printf "Result: Not target\n"
     else Tv.run nparams src_grp tgt_grp)
-  else
-    let test_ir_p = source |> Option.get in
-    let test_grp = IR.create_from_ir_file test_ir_p in
-    if unknown_op_exists test_grp then Printf.printf "Result: Not target\n"
-    else Tv.check_ub_semantic nparams check_type test_grp
+  else if String.length check_ub <> 0 then
+    let input_ir_p = check_ub in
+    let input_grp = IR.create_from_ir_file input_ir_p in
+    if unknown_op_exists input_grp then Printf.printf "Result: Not target\n"
+    else Tv.check_ub_semantic nparams check_type input_grp
+  else failwith "must give option 'verify' or '--check-ub'"
 
 let () = main ()
