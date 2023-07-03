1b270d4b468fc7a9adf1064b986b71e1acc706b4
doit-man <kjs000804@naver.com>
2023-07-03 19:01:32 +0900

    [WASM] fix from_f32string

--- lib/utils/z3utils.ml
+++ lib/utils/z3utils.ml
@@ -282,7 +282,9 @@ module BitVecVal = struct
   let from_istring_trunc ?(len = !bvlen) str = BV.mk_numeral ctx str len
 
   let from_f32string str =
-    Float.from_string ~sort:Float.single_sort str |> Float.to_ieee_bv
+    match str with
+    | "nan" | "-nan" -> Float.nan ~sort:Float.single_sort () |> Float.to_ieee_bv
+    | _ -> Float.from_string ~sort:Float.single_sort str |> Float.to_ieee_bv
 
   let from_f64string str =
     Float.from_string ~sort:Float.double_sort str |> Float.to_ieee_bv
