f05c8a9aff0556c197c73537e9e10ac540ff4bf5
Hhro <seungwan.kwon@prosys.kr>
2023-07-04 08:48:49 +0000

    [CI] use cached-opam

--- build.sh
+++ build.sh
@@ -2,25 +2,25 @@
 
 NCPU="$(getconf _NPROCESSORS_ONLN 2>/dev/null || echo 1)"
 OCAML_VERSION="4.13.1"
-JSTV_OPAM_SWITCH="jstv-4.13.1"
+TURBO_TV_OPAM_SWITCH="turbo-tv"
 opam update
 opam init --compiler=$OCAML_VERSION -j $NCPU --no-setup
 
 switch_exists=no
 for installed_switch in $(opam switch list --short); do
-  if [[ "$installed_switch" == "$JSTV_OPAM_SWITCH" ]]; then
+  if [[ "$installed_switch" == "$TURBO_TV_OPAM_SWITCH" ]]; then
     switch_exists=yes
     break
   fi
 done
 
 if [ "$switch_exists" = "no" ]; then
-  opam switch create $JSTV_OPAM_SWITCH $OCAML_VERSION
+  opam switch create $TURBO_TV_OPAM_SWITCH $OCAML_VERSION
 else
-  opam switch $JSTV_OPAM_SWITCH
+  opam switch $TURBO_TV_OPAM_SWITCH
 fi
 
-eval $(SHELL=bash opam env --switch=$JSTV_OPAM_SWITCH)
+eval $(SHELL=bash opam env --switch=$TURBO_TV_OPAM_SWITCH)
 opam install core_unix cmdliner dune ocamlgraph z3 ocamlformat=0.22.4 oUnit
 
 if [ "$1" = "gen" ]; then
